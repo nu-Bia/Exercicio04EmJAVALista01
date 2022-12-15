1e150f81dfb79fe6023bcb31ac8b367ccf05b826
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-15 16:25:39 -0300

    Add files via upload

---
+++ Exercicio04.java
@@ -0,0 +1,22 @@
+//4) Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a subtração dos valores.
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+public static void main(String[]args) {
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Digite o primeiro valor");
+
+int n1= sc.nextInt();
+
+System.out.println("Digite o segundo valor que deseja subtrair");
+
+int n2= sc.nextInt();
+
+System.out.println(n1-n2);
+
+}}
+
